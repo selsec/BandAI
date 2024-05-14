d94194be9aa46dfe9e2961eb28c720a47d682ff4
Steve T <steve@pantherprideboosters.org>
2024-05-14 18:45:21 -0400

    test

--- Main.js
+++ Main.js
@@ -1,20 +1,13 @@
 function onOpen() {
-    var ui = SpreadsheetApp.getUi();
-    ui.createMenu('BandAI Functions')
-      .addItem('Import Your Roster', 'importRoster')
-      .addItem('Calculate Begining Dues', 'calculateStartDues')
-      .addItem('Create/Update Uniform Order', 'uniformOrder')
-      .addItem('Create Bus Roster', 'createBusRoster')
-      .addItem('Update Uniform Measurements', 'importUniformUpdates')
-      .addSubMenu(ui.createMenu('Settings and Maintenance')
-        .addItem('Set Section Colors', 'setUniformColors')
-        .addItem('Sort Sheets', 'sortSheetsAlphabetically')
-      .addToUi();
-
-
-  
+  var ui = SpreadsheetApp.getUi();
+  ui.createMenu('BandAI Functions')
+    .addItem('Import Your Roster', 'importRoster')
+    .addItem('Calculate Beginning Dues', 'calculateStartDues')
+    .addItem('Create/Update Uniform Order', 'uniformOrder')
+    .addItem('Create Bus Roster', 'createBusRoster')
+    .addItem('Update Uniform Measurements', 'importUniformUpdates')
+    .addSubMenu(ui.createMenu('Settings and Maintenance')
+      .addItem('Set Section Colors', 'setUniformColors')
+      .addItem('Sort Sheets', 'sortSheetsAlphabetically'))
+    .addToUi();
 }
-
-
-
-  
